870e917f6ab04a1cefd2444c91225ef5c50705ee
ethan9452 <ethan9452@gmail.com>
2016-10-30 23:02:57 -0700

    i am masa onoo

--- packet.py
+++ packet.py
@@ -1,8 +1,26 @@
 def generate_id():
     return 1
 
-class Packet:
+class Packet(object):
     def __init__(self, source, destination):
         self.id = generate_id()
         self.source = source
         self.destination = destination
+
+class DataPacket(Packet):
+	"""TODO: Data Packet"""
+	size = 8192
+	def __init__(self, id, source, destination):
+		super(DataPacket, self).__init__(id=id, source=source, destination=destination)
+
+class AcknowledgementPacket(Packet):
+	"""TODO: Acknowledgement Packet"""
+	size = 512
+	def __init__(self, id, source, destination):
+		super(AcknowledgementPacket, self).__init__(id=id, source=source, destination=destination)
+
+class RouterPacket(Packet):
+	"""TODO: Router Packet"""
+	size = 512
+	def __init__(self, id, source, destination):
+		super(RouterPacket, self).__init__(source=source, destination=destination)
