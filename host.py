58407cd827b4e5bb306db74d48b29b7e8416c818
ethan9452 <ethan9452@gmail.com>
2016-10-30 21:49:32 -0700

    added ip

--- host.py
+++ host.py
@@ -1,6 +1,7 @@
 class Host:
-    def __init__(self, link):
+    def __init__(self, link, ip):
         self.link = link
+        self.ip = ip
 
     def send(self, packet):
         self.link.send(packet)
