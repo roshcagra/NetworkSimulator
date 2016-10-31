eebc098113dac8f4279f7ae4c68ac752a3ab412f
roshcagra <roshan.agrawal95@gmail.com>
2016-10-30 22:53:23 -0700

    Flows

--- simulator.py
+++ simulator.py
@@ -1,11 +1,16 @@
 import simpy
 from host import Host
+from packet import DataPacket
+from link import Link
 
-def example(env):
-    value = yield env.timeout(1, value=42)
-    print('now=%d, value=%d' % (env.now, value))
+data = 1.6 * (10 ** 8)
+devices = [Host(0), Host(1)]
+links = [Link(0, 1)]
+
+def flow(env, data, start, source, destination):
+    yield env.timeout(start)
+    devices[source].send(data, destination)
 
 env = simpy.Environment()
-p = env.process(example(env))
-p2 = env.process(example(env))
+p = env.process(flow(env, data, 1, 0, 1))
 env.run()
