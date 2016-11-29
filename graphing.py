26b0f79c3cf007ca6b9fbff75edd8ea5da9751d5
ethan9452 <ethan9452@gmail.com>
2016-11-29 10:42:47 -0800

    hahaha

--- graphing.py
+++ graphing.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 class Graph:
     def __init__(self, title, filename):
--- utils.py
+++ utils.py
@@ -6,15 +6,17 @@ def flow(data, start, source, destination, sim_env):
     sim_env.process(source.start_flow(data=data, destination=destination, env=sim_env))
 
 def dynamic_routing(devices, interval, sim_env):
-    short_interval = 1
     while True:
-        for zzzz in range(20):
+
+        for _ in range(4):
+
             for device in devices:
                 if isinstance(device, Router):
                     device.send_router(sim_env)
-            yield sim_env.timeout(short_interval)
+            yield sim_env.timeout(12)
 
         yield sim_env.timeout(interval)
+
 
 
         if all_events_processed(sim_env):
@@ -27,24 +29,3 @@ def all_events_processed(sim_env):
     return sim_env.peek() == simpy.core.Infinity
 
 
-
-
-
-
-
-
-# #####################################################
-# TODO delete before presentation
-# can read this if wanna know what i'm doing with the all_events_processed funcion
-# def print_bogus(bogus, env):
-#     for i in range(100):
-#         print(bogus)
-#         yield env.timeout(10)
-
-# def end_when_other(bogus, env, p_to_end):
-#     while True:
-#         print(bogus)
-#         yield env.timeout(10)
-#         if p_to_end.processed == True:
-#             print(str(p_to_end) + 'has finished  procesing!')
-#             break
